af0d1a5ecae1c11c7f88eaead33f879b5866fba0
VODTV - 工具链 <2236639958@qq.com>
2023-12-16 02:18:16 +0800

    Update readme.ts

--- src/readme.ts
+++ src/readme.ts
@@ -1,87 +1,91 @@
-import fs from "fs";
-import path from "path";
+import fs from "fs"
+import path from "path"
 
-import { handle_m3u } from "./sources";
+import { handle_m3u } from "./sources"
 
 export interface IREADMESource {
-  name: string;
-  f_name: string;
-  count?: number | undefined;
+    name: string
+    f_name: string
+    count?: number | undefined
 }
 
-export type TREADMESources = IREADMESource[];
+export type TREADMESources = IREADMESource[]
 
 export const updateChannelList = (
-  name: string,
-  f_name: string,
-  m3u: string
+    name: string,
+    f_name: string,
+    m3u: string
 ) => {
-  const list_temp_p = path.join(path.resolve(), "LIST.temp.md");
-  const list = fs.readFileSync(list_temp_p, "utf8").toString();
+    const list_temp_p = path.join(path.resolve(), "LIST.temp.md")
+    const list = fs.readFileSync(list_temp_p, "utf8").toString()
 
-  const m3uArray = handle_m3u(m3u);
-  const channelRegExp = /\#EXTINF:-1([^,]*),(.*)/;
-  let i = 1;
-  let channels: Array<string>[] = [];
-  while (i < m3uArray.length) {
-    const reg = channelRegExp.exec(m3uArray[i]) as RegExpExecArray;
-    channels.push([reg[2].replace(/\|/g, "").trim(), m3uArray[i + 1]]);
-    i += 2;
-  }
+    const m3uArray = handle_m3u(m3u)
+    const channelRegExp = /\#EXTINF:-1([^,]*),(.*)/
+    let i = 1
+    let channels: Array<string>[] = []
+    while (i < m3uArray.length) {
+        const reg = channelRegExp.exec(m3uArray[i]) as RegExpExecArray
+        channels.push([reg[2].replace(/\|/g, "").trim(), m3uArray[i + 1]])
+        i += 2
+    }
 
-  const after = list
-    .replace(
-      "<!-- list_title_here -->",
-      `# List for **${name}**\n\n> M3U: <https://vodtv.cn/iptv/${f_name}.m3u>, TXT: <https://vodtv.cn/iptv/txt/${f_name}.txt>`
-    )
-    .replace(
-      "<!-- channels_here -->",
-      `${channels
-        .map(
-          (c, idx) =>
-            `| ${idx + 1} | ${c[0].replace("|", "")} | [${c[0].replace(
-              "|",
-              ""
-            )}](${c[1]}) |`
+    const after = list
+        .replace(
+            "<!-- list_title_here -->",
+            `# List for **${name}**\n\n> M3U: <https://vodtv.cn/iptv/${f_name}.m3u>, TXT: <https://vodtv.cn/iptv/txt/${f_name}.txt>`
         )
-        .join("\n")}\n\nUpdated at **${new Date()}**`
-    );
+        .replace(
+            "<!-- channels_here -->",
+            `${channels
+                .map(
+                    (c, idx) =>
+                        `| ${idx + 1} | ${c[0].replace(
+                            "|",
+                            ""
+                        )} | [${c[0].replace("|", "")}](${c[1]}) |`
+                )
+                .join("\n")}\n\nUpdated at **${new Date()}**`
+        )
 
-  const list_p = path.join(path.resolve(), "m3u", "list");
+    const list_p = path.join(path.resolve(), "m3u", "list")
 
-  if (!fs.existsSync(list_p)) {
-    fs.mkdirSync(list_p);
-  }
+    if (!fs.existsSync(list_p)) {
+        fs.mkdirSync(list_p)
+    }
 
-  fs.writeFileSync(path.join(list_p, `${f_name}.list.md`), after);
-};
+    fs.writeFileSync(path.join(list_p, `${f_name}.list.md`), after)
+}
 
 export const updateReadme = (
-  sources: TREADMESources,
-  counts: Array<number | undefined>
+    sources: TREADMESources,
+    counts: Array<number | undefined>
 ) => {
-  const readme_temp_p = path.join(path.resolve(), "README.temp.md");
-  const readme = fs.readFileSync(readme_temp_p, "utf8").toString();
+    const readme_temp_p = path.join(path.resolve(), "README.temp.md")
+    const readme = fs.readFileSync(readme_temp_p, "utf8").toString()
 
-  const after = readme.replace(
-    "<!-- channels_here -->",
-    `${sources
-      .map(
-        (, idx) =>
-          `| ${s.name} | <https://vodtv.cn/iptv/${
-            s.f_name
-          }.m3u> <br> <https://vodtv.cn/iptv/txt/${s.f_name}.txt> | [List for ${
-            s.name
-          }](https://vodtv.cn/iptv/list/${s.f_name}.list) | ${
-            counts[idx] === undefined ? "update failed" : counts[idx]
-          } |`
-      )
-      .join("\n")}\n\nUpdated at **${new Date()}**`
-  );
+    const after = readme.replace(
+        "<!-- channels_here -->",
+        `${sources
+            .map(
+                (, idx) =>
+                    `| ${s.name} | <https://vodtv.cn/iptv/${
+                        s.f_name
+                    }.m3u> <br> <https://vodtv.cn/iptv/txt/${
+                        s.f_name
+                    }.txt> | [List for ${s.name}](https://vodtv.cn/iptv/list/${
+                        s.f_name
+                    }.list) | ${
+                        counts[idx] === undefined
+                            ? "update failed"
+                            : counts[idx]
+                    } |`
+            )
+            .join("\n")}\n\nUpdated at **${new Date()}**`
+    )
 
-  if (!fs.existsSync(path.join(path.resolve(), "m3u"))) {
-    fs.mkdirSync(path.join(path.resolve(), "m3u"));
-  }
+    if (!fs.existsSync(path.join(path.resolve(), "m3u"))) {
+        fs.mkdirSync(path.join(path.resolve(), "m3u"))
+    }
 
-  fs.writeFileSync(path.join(path.resolve(), "m3u", "README.md"), after);
-};
+    fs.writeFileSync(path.join(path.resolve(), "m3u", "README.md"), after)
+}
