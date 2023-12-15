43203136019d5cc91aa0df62309c3d3d42b802fe
VODTV - 工具链 <2236639958@qq.com>
2023-12-16 00:44:31 +0800

    add

--- src/readme.ts
+++ src/readme.ts
@@ -1,6 +1,8 @@
 import fs from "fs";
 import path from "path";
 
+import { handle_m3u } from "./sources";
+
 export interface IREADMESource {
   name: string;
   f_name: string;
@@ -17,20 +19,20 @@ export const updateChannelList = (
   const list_temp_p = path.join(path.resolve(), "LIST.temp.md");
   const list = fs.readFileSync(list_temp_p, "utf8").toString();
 
-  const m3uArray = m3u.split("\n");
+  const m3uArray = handle_m3u(m3u);
   const channelRegExp = /\#EXTINF:-1([^,]*),(.*)/;
   let i = 1;
   let channels: Array<string>[] = [];
   while (i < m3uArray.length) {
     const reg = channelRegExp.exec(m3uArray[i]) as RegExpExecArray;
-    channels.push([reg[2].trim(), m3uArray[i + 1]]);
+    channels.push([reg[2].replace(/\|/g, "").trim(), m3uArray[i + 1]]);
     i += 2;
   }
 
   const after = list
     .replace(
       "<!-- list_title_here -->",
-      `# List for **${name}**\n\n> M3U: <https://vodtv.cn/iptv/${f_name}.m3u>, TXT: <https://vodtv.cn/iptv/txt/${f_name}.txt>`
+      `# List for **${name}**\n\n> M3U: <https://m3u.ibert.me/${f_name}.m3u>, TXT: <https://m3u.ibert.me/txt/${f_name}.txt>`
     )
     .replace(
       "<!-- channels_here -->",
@@ -65,12 +67,12 @@ export const updateReadme = (
     "<!-- channels_here -->",
     `${sources
       .map(
-        (, idx) =>
-          `| ${s.name} | <https://vodtv.cn/iptv/${
+        (s, idx) =>
+          `| ${s.name} | <https://m3u.ibert.me/${
             s.f_name
-          }.m3u> <br> <https://vodtv.cn/iptv/txt/${s.f_name}.txt> | [List for ${
+          }.m3u> <br> <https://m3u.ibert.me/txt/${s.f_name}.txt> | [List for ${
             s.name
-          }](https://vodtv.cn/iptv/list/${s.f_name}.list) | ${
+          }](https://m3u.ibert.me/list/${s.f_name}.list) | ${
             counts[idx] === undefined ? "update failed" : counts[idx]
           } |`
       )
--- src/sources/utils.ts
+++ src/sources/utils.ts
@@ -1,15 +1,14 @@
-import * as OpenCC from "opencc-js"
+import * as OpenCC from "opencc-js";
 
 export interface ISource {
-    name: string
-    f_name: string
-    url: string
-    filter?: (raw: string) => [string, number]
+  name: string;
+  f_name: string;
+  url: string;
+  filter?: (raw: string) => [string, number];
 }
 
-export type TSources = ISource[]
+export type TSources = ISource[];
 
-<<<<<<< HEAD
 export const converter = OpenCC.Converter({ from: "hk", to: "cn" });
 
 export const handle_m3u = (r: string) => {
@@ -26,21 +25,3 @@ export const replace_github_rawcontent = (url: string) => {
     "https://ghproxy.net/https://raw.githubusercontent.com/"
   );
 };
-=======
-export const converter = OpenCC.Converter({ from: "hk", to: "cn" })
-
-export const handle_m3u = (r: string) => {
-    return r
-        .trim()
-        .replace(/\r/g, "")
-        .split("\n")
-        .filter((r) => !!r)
-}
-
-export const replace_github_rawcontent = (url: string) => {
-    return url.replace(
-        "https://raw.githubusercontent.com/",
-        "https://ghproxy.net/https://raw.githubusercontent.com/"
-    )
-}
->>>>>>> 466b3580b739f7dcdb4f7e9081bf73709966c9bd
