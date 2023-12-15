6bf397e72b6dd9588e4b3483767d309f821a73d1
VODTV - 工具链 <2236639958@qq.com>
2023-12-15 22:45:23 +0800

    Update utils.ts

--- src/sources/utils.ts
+++ src/sources/utils.ts
@@ -1,12 +1,27 @@
-import * as OpenCC from "opencc-js";
+import * as OpenCC from "opencc-js"
 
 export interface ISource {
-  name: string;
-  f_name: string;
-  url: string;
-  filter?: (raw: string) => [string, number];
+    name: string
+    f_name: string
+    url: string
+    filter?: (raw: string) => [string, number]
 }
 
-export type TSources = ISource[];
+export type TSources = ISource[]
 
-export const converter = OpenCC.Converter({ from: "hk", to: "cn" });
+export const converter = OpenCC.Converter({ from: "hk", to: "cn" })
+
+export const handle_m3u = (r: string) => {
+    return r
+        .trim()
+        .replace(/\r/g, "")
+        .split("\n")
+        .filter((r) => !!r)
+}
+
+export const replace_github_rawcontent = (url: string) => {
+    return url.replace(
+        "https://raw.githubusercontent.com/",
+        "https://ghproxy.net/https://raw.githubusercontent.com/"
+    )
+}
