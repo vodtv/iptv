693939a7b9b3eb7db8a002e2b2420fb6765a9a44
VODTV - 工具链 <2236639958@qq.com>
2023-12-16 20:59:27 +0800

    add

--- src/sources/index.ts
+++ src/sources/index.ts
@@ -1,24 +1,24 @@
-export * from "./epg_pw"
-export * from "./iptv_org"
-export * from "./yang_m3u"
-export * from "./yuechan_live"
-export * from "./fanmingming_live"
-export * from "./utils"
+export * from "./epg_pw";
+export * from "./iptv_org";
+export * from "./yang_m3u";
+export * from "./yuechan_live";
+export * from "./fanmingming_live";
+export * from "./utils";
 
 import {
-    epg_pw_sources,
-    iptv_org_sources,
-    iptv_org_stream_sources,
-    yang_m3u_sources,
-    yuechan_live_sources,
-    fanmingming_live_sources
-} from "."
+  epg_pw_sources,
+  iptv_org_sources,
+  iptv_org_stream_sources,
+  yang_m3u_sources,
+  yuechan_live_sources,
+  fanmingming_live_sources,
+} from ".";
 
 export const sources = [
-    ...fanmingming_live_sources,
-    ...epg_pw_sources,
-    ...yuechan_live_sources,
-    ...yang_m3u_sources,
-    ...iptv_org_sources,
-    ...iptv_org_stream_sources,
-]
+  ...fanmingming_live_sources,
+  ...epg_pw_sources,
+  ...yuechan_live_sources,
+  ...yang_m3u_sources,
+  ...iptv_org_sources,
+  ...iptv_org_stream_sources,
+];
